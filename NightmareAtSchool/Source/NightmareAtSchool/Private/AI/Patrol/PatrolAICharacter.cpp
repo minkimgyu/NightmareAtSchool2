38928fc96c082164ebd7a6673d629b1bce28a992
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-12-20 01:09:22 +0900

    Fix file path

--- NightmareAtSchool/Source/NightmareAtSchool/Private/AI/Patrol/PatrolAICharacter.cpp
+++ NightmareAtSchool/Source/NightmareAtSchool/Private/AI/Patrol/PatrolAICharacter.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "AI/PatrolAIController.h"          // ★ 반드시 필요
+#include "AI/Patrol/PatrolAIController.h"          // ★ 반드시 필요
 #include "GameFramework/Character.h"         // 보통 포함되어 있음
-#include "AI/PatrolAICharacter.h"
+#include "AI/Patrol/PatrolAICharacter.h"
 
 // Sets default values
 APatrolAICharacter::APatrolAICharacter()
--- NightmareAtSchool/Source/NightmareAtSchool/Private/AI/Patrol/PatrolManager.cpp
+++ NightmareAtSchool/Source/NightmareAtSchool/Private/AI/Patrol/PatrolManager.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "AI/PatrolPoint.h"
+#include "AI/Patrol/PatrolPoint.h"
 #include "Kismet/GameplayStatics.h"
 
-#include "AI/PatrolManager.h"
+#include "AI/Patrol/PatrolManager.h"
 
 // Sets default values
 APatrolManager::APatrolManager()
